67d25db196c61bcd0ddc9be3cc920c8de7dd8301
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-04-12 15:35:11 -0300

    feat: fiz o header do site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Login from "./components/login/login.js"
-import postGenerate from "./components/postGenerate/postGenerate.js"
+import Header from "./components/header/header.js"
+import PostGenerate from "./components/postGenerate/postGenerate.js"
 import ToolBar from "./components/toolBar/toolBar.js";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <ToolBar />
     </div>
   );
--- src/components/toolBar/toolBar.js
+++ src/components/toolBar/toolBar.js
@@ -3,10 +3,10 @@ import "./toolBar.css";
 
 function ToolBar() {
   return (
-    <aside className="toolBar">
-      <button className="fa-solid fa-house aside__Button"></button>
-      <button className="fa-solid fa-user aside__Button"></button>
-    </aside>
+    <menu className="toolBar">
+      <button className="fa-solid fa-house menu__Button"></button>
+      <button className="fa-solid fa-user menu__Button"></button>
+    </menu>
   );
 }
 
