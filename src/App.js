9292e77e7bce7465c6371c3023bb63e79f964674
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-04-12 12:13:43 -0300

    feat: teste para iniciar projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
+import login from "./components/login/login.js"
+import toolBar from "./components/toolBar/toolBar.js";
+import postGenerate from "./components/postGenerate/postGenerate.js"
 
 function App() {
   return (
     <div className="App">
+      <toolBar />
     </div>
   );
 }
---
+++ src/components/toolBar/toolBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./toolBar.css"
+
+function toolBar() {
+    return (
+    <header>
+        <h1>Teste React</h1>
+    </header>
+    )
+}
+
+export default toolBar;
